bbfb1577af49dbf7ca34810c7b5242f68086bb2f
StefanButacu <79003864+StefanButacu@users.noreply.github.com>
2021-07-15 23:47:47 +0300

    Add files via upload

---
+++ ADT Ordered Map(1)/Lab4/main.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+#include "TestScurt.h"
+#include "TestExtins.h"
+
+/*Să se implementeze în C++ un anumit container de date (TAD) folosind o anumită 
+reprezentare (indicată) și o listă înlănțuită (LSI – lista simplu înlănțuită, LDI – lista dublu înlănțuită) 
+ca structură de date, cu reprezentarea înlănțuirilor pe tablou dinamic. 
+*/
+/*
+23. TAD Lista Ordonată(interfața cu TPozitie = Iterator) – reprezentare sub forma unei LSI cu
+elementele ordonate în raport cu o relație de ordine R.
+*/
+
+int main() {
+    testAll();
+    testAllExtins();
+    std::cout << "Finished LP Tests!" << std::endl;
+}
